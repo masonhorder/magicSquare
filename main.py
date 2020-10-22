1687a08169074c02f6f3e98706169291533b29cc
masonhorder <masonhorder@gmail.com>
2020-10-22 08:38:07 -0700

    final

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 def magicSquare(inputArray):
-    totalConversions = 0
-    possibleValues = []
-    numberOfValues = []
-    for row in inputArray:
-        rowTotal = 0
-        for item in row:
-            rowTotal += inputArray[inputArray.index(row)][row.index(item)]
-        if rowTotal in possibleValues:
-            numberOfValues[possibleValues.index(rowTotal)] += 1
-        else:
-            possibleValues.append(rowTotal)
-            numberOfValues.append(1)
+    minCost = 0
+    possibleSolutions = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+    ]
+    indexOfSolutions = 0
+    for magicSquare in possibleSolutions:
+        index1 = 0
+        index2 = 0
+        currentCost = 0
+        for section in magicSquare:
+            for number in section:
+                if inputArray[index1][index2] != number:
+
+                    currentCost += abs(inputArray[index1][index2]-number)
+                
+                index2 += 1
+
+            index1 += 1
+            index2 = 0
+
+        if indexOfSolutions == 0:
+            minCost = currentCost
+        else:    
+            if currentCost < minCost:
+                minCost = currentCost
+                
+        indexOfSolutions += 1
+
+    
+
+    
 
 
-    return possibleValues
+    return minCost
 
 
-print(magicSquare([[1,2,3],[4,5,6],[7,8,9]]))
+print(magicSquare([[4,8,2],[4,5,7],[6,1,6]]))
 
